0d6867963f95d851bcc6d3d5107e3acaadf5c9b0
Kundan Keshri <keshrikundan94@gmail.com>
2025-12-24 01:14:59 +0530

    Time: 3 ms (88.05%), Space: 48.3 MB (17.49%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -2,28 +2,17 @@
     public boolean searchMatrix(int[][] matrix, int target) {
         int n=matrix.length;
         int m=matrix[0].length;
-        for(int i=0;i<n;i++){
-             if(matrix[i][0]<=target && target<=matrix[i][m-1]){
-                if(BS(matrix[i],target)){
-                    return true;
-                }
-             }
-        }
-        return false;
-    }
-    private boolean BS(int[] nums,int target){
-        int low=0;
-        int high=nums.length-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target){
+         int row=0;
+         int col=m-1;
+         while(row<n && col>=0){
+            if(matrix[row][col]==target){
                 return true;
-            }else if(nums[mid]<target){
-                low=mid+1;
+            }else if(matrix[row][col]<target){
+                row++;
             }else{
-                high=mid-1;
+                col--;
             }
-        }
+         }
         return false;
     }
 }
