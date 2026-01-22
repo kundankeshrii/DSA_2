821eaed736521d18f916bfc0789fc6eeed5b1116
Kundan Keshri <keshrikundan94@gmail.com>
2026-01-22 23:38:19 +0530

    Time: 3 ms (91.12%), Space: 46.7 MB (53.72%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,18 +1,20 @@
 class Solution {
-    Set<List<Integer>> set=new HashSet<>();
+    List<List<Integer>> ans=new ArrayList<>();
     boolean[] used;
     public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
         used=new boolean[nums.length];
         backTrack(nums,new ArrayList<>());
-        return new ArrayList<>(set);
+        return ans;
     }
     private void backTrack(int[] nums,List<Integer>path){
         if(path.size()==nums.length){
-            set.add(new ArrayList<>(path));
+            ans.add(new ArrayList<>(path));
             return;
         }
         for(int i=0;i<nums.length;i++){
             if(used[i]) continue;
+            if(i>0 && nums[i]==nums[i-1] && !used[i-1]) continue;
             used[i]=true;
             path.add(nums[i]);
             backTrack(nums,path);
