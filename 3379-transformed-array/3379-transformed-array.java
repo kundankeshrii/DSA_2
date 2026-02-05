9b5c429198d1243b258237cb74b536cfa93ac3f4
Kundan Keshri <keshrikundan94@gmail.com>
2026-02-06 00:32:13 +0530

    Time: 1 ms (98.72%), Space: 46.9 MB (13.78%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.java
+++ 3379-transformed-array/3379-transformed-array.java
@@ -3,17 +3,8 @@ class Solution {
         int n=nums.length;
         int[] result=new int[n];
         for(int i=0;i<n;i++){
-            if(nums[i]>0){
                 result[i]=nums[(i+nums[i]%n+n)%n];
             }
-            else if(nums[i]<0){
-                int b=Math.abs(nums[i])%n;
-                result[i]=nums[(i-b+n)%n];
-            }
-            else {
-                result[i]=nums[i];
-            }
-        }
         return result;
     }
 }
