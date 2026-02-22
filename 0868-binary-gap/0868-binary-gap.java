738e59f5d16b0602bdfc599f8d209fc7afe3c9ec
Kundan Keshri <keshrikundan94@gmail.com>
2026-02-22 18:51:31 +0530

    Time: 0 ms (100%), Space: 42.3 MB (48.75%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.java
+++ 0868-binary-gap/0868-binary-gap.java
@@ -1,14 +1,12 @@
 class Solution {
     public int binaryGap(int n) {
-        int curr=0;
         int prev=-1;
         int result=0;
-        while(n>0){
+        for(int curr=0;curr<32;curr++){
             if((n&1)>0){
                 result=(prev!=-1)?Math.max(result,curr-prev):result;
                 prev=curr;
             }
-            curr++;
             n>>=1;
         }
         return result;
