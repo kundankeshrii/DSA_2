ca2f40ddb3ff5483911b598627f54c0f519fe328
Kundan Keshri <keshrikundan94@gmail.com>
2026-03-14 23:47:34 +0530

    Time: 1 ms (100%), Space: 43.3 MB (73.16%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,22 +1,24 @@
 class Solution {
     public String getHappyString(int n, int k) {
+        int[] cnt={0};
         StringBuilder curr=new StringBuilder();
-        List<String>result=new ArrayList<>();
-        solve(curr,result,n);
-        if(result.size()<k){
-            return "";
-        }
-        return result.get(k-1);
+        String[] result={""};
+        solve(curr,k,cnt,result,n);
+        return result[0];
     }
-    private void solve(StringBuilder curr,List<String>result,int n){
+    private void solve(StringBuilder curr,int k,int[] cnt,String[]result,int n){
         if(curr.length()==n){
-            result.add(curr.toString());
+             cnt[0]++;
+             if(cnt[0]==k){
+                result[0]=curr.toString();
+             }
             return;
         }
         for(char ch='a';ch<='c';ch++){
             if(curr.length()>0 && curr.charAt(curr.length()-1)==ch) continue;
             curr.append(ch);
-            solve(curr,result,n);
+            solve(curr,k,cnt,result,n);
+            if(!result[0].isEmpty()) return;
             curr.deleteCharAt(curr.length()-1);
         }
     }
