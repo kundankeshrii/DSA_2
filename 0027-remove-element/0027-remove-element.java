e48f9641c2018b78d4717df4e0af2555ec966067
Kundan Keshri <keshrikundan94@gmail.com>
2026-03-24 03:00:34 +0530

    Time: 3 ms (2.07%), Space: 43.8 MB (17.74%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n=nums.length;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==val){
+                nums[i]=51;
+                cnt++;
+            }
+        }
+        Arrays.sort(nums);
+        return n-cnt;
+    }
+}
