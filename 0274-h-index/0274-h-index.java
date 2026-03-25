c5b4925a1b0c57bed06a10f20c3a04a45c0f9769
Kundan Keshri <keshrikundan94@gmail.com>
2026-03-25 17:39:43 +0530

    Time: 5 ms (68.74%), Space: 43.9 MB (11.31%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,18 +1,13 @@
 class Solution {
     public int hIndex(int[] citations) {
         int n=citations.length;
-        int hI=0;
-        for(int i=1;i<=n;i++){
-            int cnt=0;
-            for(int j=0;j<n;j++){
-                if(i<=citations[j]){
-                    cnt++;
-                }
-            }
-            if(cnt>=i){
-                hI=i;
+        Arrays.sort(citations);
+        for(int i=0;i<n;i++){
+            int h=n-i;
+            if(citations[i]>=h){
+                return h;
             }
         }
-        return hI;
+        return 0;
     }
 }
