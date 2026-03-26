c3a7b21ddf945e40b08e243303eb24c29f6a7332
Kundan Keshri <keshrikundan94@gmail.com>
2026-03-26 18:34:07 +0530

    Time: 3 ms (86.26%), Space: 47.5 MB (80.01%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int[] arr=new int[n];
+        Arrays.fill(arr,1);
+        //aise questions ko 2 pass ke help se karte hai
+        //left se right and for cross check right to left
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                arr[i]=arr[i-1]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                arr[i]=Math.max(arr[i],arr[i+1]+1);
+            }
+        }
+         
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+    return sum;
+    }
+}
