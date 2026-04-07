e69907362515fae653dd596517eeee8e694d9ca7
Kundan Keshri <keshrikundan94@gmail.com>
2026-04-07 22:24:30 +0530

    Time: 6 ms (99.55%), Space: 49.5 MB (87.98%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>>mpp=new HashMap<>();
+        for(String s:strs){
+            char[] arr=s.toCharArray();
+            Arrays.sort(arr);
+            String key=new String(arr);
+            mpp.putIfAbsent(key,new ArrayList<>());
+            mpp.get(key).add(s);
+        }
+        return new ArrayList<>(mpp.values());
+    }
+}
