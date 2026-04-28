65ff770b3155904b8185bb93ca3d3e5e8f886a95
Kundan Keshri <keshrikundan94@gmail.com>
2026-04-28 22:21:07 +0530

    Time: 189 ms (68.23%), Space: 274.2 MB (60.17%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,55 @@
+class WordDictionary {
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEnd = false;
+    }
+
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode node = root;
+
+        for (char ch : word.toCharArray()) {
+            int idx = ch - 'a';
+
+            if (node.children[idx] == null) {
+                node.children[idx] = new TrieNode();
+            }
+
+            node = node.children[idx];
+        }
+
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+
+    private boolean dfs(String word, int index, TrieNode node) {
+        if (node == null) return false;
+
+        if (index == word.length()) {
+            return node.isEnd;
+        }
+
+        char ch = word.charAt(index);
+
+        if (ch == '.') {
+            // try all possible paths
+            for (TrieNode child : node.children) {
+                if (dfs(word, index + 1, child)) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            return dfs(word, index + 1, node.children[ch - 'a']);
+        }
+    }
+}
