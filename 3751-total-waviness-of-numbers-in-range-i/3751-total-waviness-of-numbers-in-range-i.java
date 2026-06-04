9904ab6088049bb6ecfcd04737ac68179cb12ea6
Kundan Keshri <keshrikundan94@gmail.com>
2026-06-04 14:13:55 +0530

    Time: 81 ms (5.95%), Space: 46.4 MB (52.04%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.java
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.java
@@ -4,21 +4,20 @@ class Solution {
         for(int i=num1;i<=num2;i++){
             int digit=(int)(Math.log10(i)+1);
             if(digit>2){
-            List<Integer>arr=new ArrayList<>();
-            int n=i;
-            while(n>0){
-                int r=n%10;
-                arr.add(r);
-                n/=10;
-            }
-            Collections.reverse(arr);
-            for(int j=1;j<arr.size()-1;j++){
-                if(arr.get(j-1)<arr.get(j) && arr.get(j)>arr.get(j+1)){
-                    cnt++;
-                }if(arr.get(j-1)>arr.get(j) && arr.get(j)<arr.get(j+1)){
-                    cnt++;
+                List<Integer>arr=new ArrayList<>();
+                int n=i;
+                while(n>0){
+                    int r=n%10;
+                    arr.add(r);
+                    n/=10;
                 }
-            }
+                Collections.reverse(arr);
+                for(int j=1;j<arr.size()-1;j++){
+                    if((arr.get(j-1)<arr.get(j) && arr.get(j)>arr.get(j+1)) ||
+                    (arr.get(j-1)>arr.get(j) && arr.get(j)<arr.get(j+1))){
+                        cnt++;
+                    } 
+                }
 
             }
         }
