0675d74e473bab58646fddfea5bd5f22bcbb6192
Kundan Keshri <keshrikundan94@gmail.com>
2026-06-08 00:22:32 +0530

    Time: 3 ms (86.89%), Space: 71.8 MB (40.42%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,25 +1,18 @@
 class Solution {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
         int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=arr[i];
+        }
+         //Average==Sum/k and required==>  sum/k>=threshold i.e sum>=threshold
         int cnt=0;
-        int l=0;
-        for(int r=0;r<arr.length;r++){
-            sum+=arr[r];
-            int avg=0;
-            if(r-l+1==k){
-               avg=sum/k;
-               if(avg>=threshold){
-                cnt++;
-               }
-            }
-            if(r-l+1>k){
-                sum-=arr[l];
-                avg=sum/k;
-                if(avg>=threshold){
-                    cnt++;
-                }
-                l++;
-            } 
+        if(sum>=threshold*k){
+            cnt++;
+        }
+        for(int i=k;i<arr.length;i++){
+            sum=sum-arr[i-k]+arr[i];  //arr[i-k]===left wala element
+                                        //arr[i]===current wala element
+            if(sum>=threshold*k) cnt++;
         }
         return cnt;
     }
