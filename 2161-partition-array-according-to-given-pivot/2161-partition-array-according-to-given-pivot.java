117a54d0667476600357acc4791a9116018215a8
Kundan Keshri <keshrikundan94@gmail.com>
2026-06-08 18:11:47 +0530

    Time: 5 ms (73.56%), Space: 159.8 MB (73.16%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,27 +1,22 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer>list1=new ArrayList<>();
-        List<Integer>list2=new ArrayList<>();
-        List<Integer>list3=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
+        int n=nums.length;
+        int[]ans=new int[n];
+        int left=0,right=n-1;
+        for(int i=0,j=n-1;i<n;i++,j--){
             if(nums[i]<pivot){
-                list1.add(nums[i]);
-            }else if(nums[i]==pivot){
-                list2.add(nums[i]);
-            }else if(nums[i]>pivot){
-                list3.add(nums[i]);
+                ans[left]=nums[i];
+                left++;
+            }
+            if(nums[j]>pivot){
+                ans[right]=nums[j];
+                right--;
             }
         }
-        for(int i=0;i<list2.size();i++){
-            list1.add(list2.get(i));
+        while(left<=right){
+            ans[left]=pivot;
+            left++;
         }
-        for(int i=0;i<list3.size();i++){
-            list1.add(list3.get(i));
-        }
-        for(int i=0;i<list1.size();i++){
-            nums[i]=list1.get(i);
-        }
-         
-        return nums;
+        return ans;
     }
 }
