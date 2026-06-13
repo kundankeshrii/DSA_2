59b08e8e1911bcfda6f3a808573e15d4f3850b1b
Kundan Keshri <keshrikundan94@gmail.com>
2026-06-13 22:15:42 +0530

    Time: 2 ms (96.85%), Space: 46.3 MB (81.55%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.java
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -1,22 +1,15 @@
 class Solution {
     public String mapWordWeights(String[] words, int[] weights) {
-        Map<Integer,Character>mpp=new HashMap<>();
-        String s="";
-        for(int i=0;i<26;i++){
-            char ch=(char)('z'-i);
-            mpp.put(i,ch);
-        }
+        StringBuilder sb=new StringBuilder();
         for(String word:words){
             int sum=0;
-             for(int i=0;i<word.length();i++){
-                char ch=word.charAt(i);
-                int idx=(int)(ch-'a');
-                sum+=weights[idx];
-             }
-             sum=sum%26;
-             char c=mpp.get(sum);
-            s=s+c;
+            for(char ch:word.toCharArray()){
+                sum+=weights[ch-'a'];
+            }
+            int mod=sum%26;
+            char finalChar=(char)('z'-mod);
+            sb.append(finalChar);
         }
-        return s;
+        return sb.toString();
     }
 }
