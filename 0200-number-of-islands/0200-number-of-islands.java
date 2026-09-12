515f59a5a66c313ea022ae19185c9e5feec62dc7
Kundan Keshri <keshrikundan94@gmail.com>
2026-09-13 04:34:40 +0530

    Time: 8 ms (10.49%), Space: 55.1 MB (5.13%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,17 +1,9 @@
-class Pair{
-    int first;
-    int second;
-    Pair(int row,int col){
-        this.first=row;
-        this.second=col;
-    }
-}
 class Solution {
     public int numIslands(char[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        int cnt=0;
         int[][] vis=new int[m][n];
+        int cnt=0;
         for(int row=0;row<m;row++){
             for(int col=0;col<n;col++){
                 if(vis[row][col]==0 && grid[row][col]=='1'){
@@ -21,28 +13,30 @@ class Solution {
             }
         }
         return cnt;
+
+
     }
-    private void bfs(int row, int col,int[][]vis,char[][] grid){
-        vis[row][col]=1;
-        Queue<Pair>q=new LinkedList<>();
-        q.add(new Pair(row,col));
+    private void bfs(int row,int col,int[][] vis,char[][] grid){
         int m=grid.length;
         int n=grid[0].length;
-        int dR[]={-1,0,1,0};
-        int dC[]={0,1,0,-1};
+        vis[row][col]=1;
+        Queue<int[]>q=new LinkedList<>();
+        q.add(new int[]{row,col});
         while(!q.isEmpty()){
-            Pair val=q.poll();
-            row=val.first;
-            col=val.second;
+            int[] arr=q.poll();
+            int r=arr[0];
+            int c=arr[1];
+
+            int[] dr={-1,0,1,0};
+            int[] dc={0,1,0,-1};
             for(int i=0;i<4;i++){
-                int nR=row+dR[i];
-                int nC=col+dC[i];
-                if(nR>=0 && nR<m && nC>=0 && nC<n && vis[nR][nC]==0 && grid[nR][nC]=='1'){
-                    vis[nR][nC]=1;
-                    q.add(new Pair(nR,nC));
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && vis[nr][nc]==0 && grid[nr][nc]=='1'){
+                    vis[nr][nc]=1;
+                    q.add(new int[]{nr,nc});
                 }
             }
         }
-
     }
 }
